8c56b296cad6473211c8d8f961fa9c82b7f46b5a
piriram <pyoram25@gmail.com>
2025-11-15 02:52:36 +0900

    Feat: 카테고리프로그레스바 컴포넌트화

---
+++ KIDK/Presentation/View/Common/ProfileImageView.swift
@@ -0,0 +1,45 @@
+//
+//  ProfileImageView.swift
+//  KIDK
+//
+//  Created by 잠만보김쥬디 on 11/15/25.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileImageView: UIView {
+    
+    private let imageView = UIImageView()
+    
+    init(assetName: String, size: CGFloat, bgColor: UIColor,iconRatio: CGFloat) {
+        super.init(frame: .zero)
+        
+        backgroundColor = bgColor
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.image = UIImage(named: assetName)
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        
+        snp.makeConstraints { make in
+            make.width.height.equalTo(size)
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.width.height.equalTo(size * iconRatio)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = bounds.width / 2
+        imageView.layer.cornerRadius = imageView.bounds.width / 2
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
